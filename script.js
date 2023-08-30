dc0281a7212f9effb5b665f2e0fcb87d67f3115f
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-08-30 22:09:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,101 @@ const scheduleData = [
         { time: 9, event: "Toán" },
         { time: 10, event: "Ngữ Văn" }
     ]
+  },
+  {
+    "day": "Tuesday",
+    "schedule": [
+        { time: 1, event: "Toán " },
+        { time: 2, event: "Toán" },
+        { time: 3, event: "TC Địa" },
+        { time: 4, event: "Tin" },
+        { time: 5, event: "Tin" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "Địa Lý" },
+        { time: 9, event: "Vật Lý" },
+        { time: 10, event: "Lịch Sử" }
+    ]
+  }
+  ,
+  {
+    "day": "Wednesday",
+    "schedule": [
+        { time: 1, event: "Hoá Học" },
+        { time: 2, event: "Vật Lý" },
+        { time: 3, event: "TC Anh Văn" },
+        { time: 4, event: "Lịch Sử" },
+        { time: 5, event: "Sinh Học" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "Thể Dục " },
+        { time: 9, event: "Ngữ Văn" },
+        { time: 10, event: "Anh Văn" }
+    ]
+  }
+  ,
+  {
+    "day": "thursday",
+    "schedule": [
+        { time: 1, event: "Địa Lý" },
+        { time: 2, event: "GDCD" },
+        { time: 3, event: "Ngữ Văn" },
+        { time: 4, event: "Toán" },
+        { time: 5, event: "TC Toán" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "Anh Văn" },
+        { time: 9, event: "Lịch Sử" },
+        { time: 10, event: "Địa Lý" }
+    ]
+  }
+  ,
+  {
+    "day": "friday",
+    "schedule": [
+        { time: 1, event: "TC Sử" },
+        { time: 2, event: "Công Nghệ" },
+        { time: 3, event: "Anh Văn" },
+        { time: 4, event: "Anh Văn" },
+        { time: 5, event: "Toán" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "GD QP-AN" },
+        { time: 9, event: "Thể Dục " },
+        { time: 10, event: "Toán" }
+    ]
+  }
+  ,
+  {
+    "day": "saturday",
+    "schedule": [
+        { time: 1, event: "Ngữ Văn" },
+        { time: 2, event: "Ngữ Văn" },
+        { time: 3, event: "Hoá Học" },
+        { time: 4, event: "SHL" },
+        { time: 5, event: "NGLL" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "" },
+        { time: 9, event: "" },
+        { time: 10, event: "" }
+    ]
+  }
+  ,
+  {
+    "day": "Sunday",
+    "schedule": [
+        { time: 1, event: "" },
+        { time: 2, event: "" },
+        { time: 3, event: "" },
+        { time: 4, event: "" },
+        { time: 5, event: "" },
+        { time: 6, event: "" },
+        { time: 7, event: "" },
+        { time: 8, event: "" },
+        { time: 9, event: "" },
+        { time: 10, event: "" }
+    ]
   }
   // Thêm dữ liệu thời khoá biểu tương tự ở đây
 ];
