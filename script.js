79c94367534d6b2c4bb24ab7b4ddd3d2ec325707
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-08-30 21:06:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const scheduleData = [
   { time: 1, event: "Chào cờ" },
   { time: 2, event: "Anh Văn" },
-  { time: 3, event: "Sinh Học" }
+  { time: 3, event: "Sinh Học" },
+  { time: 4, event: "Lịch Sử" },
+  { time: 5, event: "Vật Lý" },
+  { time: 6, event: "" },
+  { time: 7, event: "" },
+  { time: 8, event: "Vật Lý" },
+  { time: 9, event: "Toán" },
+  { time: 10, event: "Ngữ Văn" },
   // Thêm dữ liệu thời khoá biểu tương tự ở đây
 ];
 
