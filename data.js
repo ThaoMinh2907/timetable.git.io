cae988bd435c0cb041b3e8d17aacf81513721ec9
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-09-05 00:04:12 +0700

    Update

--- data.js
+++ data.js
@@ -198,3 +198,228 @@ const mega_data = [
         ],
     },
 ];
+
+// // Tạo một đối tượng FileReader
+// var reader = new FileReader();
+
+// // Định nghĩa hàm xử lý khi tệp được đọc
+// reader.onload = function (event) {
+//     // Đọc nội dung của tệp
+//     var fileContent = event.target.result;
+
+//     // Chuyển đổi nội dung thành đối tượng JavaScript
+//     var jsonData = JSON.parse(fileContent);
+
+//     // Sử dụng dữ liệu đã đọc
+//     console.log(jsonData);
+// };
+
+// // Đọc tệp JSON
+// reader.readAsText(file);
+
+// function hcmue() {
+//     // Lấy bảng theo id
+//     var table = document.getElementById(
+//         "ContentPlaceHolder1_ctl00_ctl00_ctl00_tblThoiKhoaBieu"
+//     );
+
+//     // Lấy tất cả các hàng trong bảng
+//     var rows = table.getElementsByTagName("tr");
+
+//     // Khởi tạo mảng chứa dữ liệu
+//     var data = [];
+
+//     // Lặp qua từng hàng (bắt đầu từ hàng thứ 1)
+//     for (var i = 1; i < rows.length; i++) {
+//         var row = rows[i];
+
+//         // Lấy tất cả các ô trong hàng
+//         var cells = row.getElementsByTagName("td");
+
+//         // Khởi tạo đối tượng dữ liệu cho hàng hiện tại
+//         var rowData = {};
+
+//         // Lấy nội dung của ô đầu tiên (phòng)
+//         rowData.phong = cells[0].innerText;
+
+//         // Lặp qua từng ô từ cột thứ 2 đến cột cuối cùng
+//         for (var j = 1; j < cells.length; j++) {
+//             var cell = cells[j];
+//             // console.log(cell)
+
+//             // Lấy nội dung của ô hiện tại
+//             // var content = cell.innerText;
+//             var temp = [];
+//             // Đặt tên thuộc tính cho đối tượng dữ liệu dựa trên cột tương ứng
+//             for (var k = 0; k < cell.childNodes.length; k++) {
+//                 temp.push(cell.childNodes[k].textContent);
+//             }
+//             rowData[j - 1] = temp;
+//         }
+
+//         // Thêm dữ liệu của hàng hiện tại vào mảng dữ liệu
+//         data.push(rowData);
+//     }
+//     // localStorage.setItem(data_hscmue, data);
+// }
+
+function parse_data(data) {
+    var temp_data = [];
+
+    // Lặp qua các cột (0, 1, 2, 3, 4, 5, 6)
+    for (let i = 0; i < 7; i++) {
+        const columnData = {};
+        var length = 0;
+        // Lặp qua các hàng trong mảng mega_data
+        for (let j = 0; j < data.length; j++) {
+            const cellContent = data[j][i];
+            if (cellContent && cellContent.length > 1) {
+                // Sử dụng index của hàng (j) làm key và cellContent làm value
+                columnData[length++] = cellContent;
+            }
+        }
+
+        // Thêm cột dữ liệu vào mảng data
+        temp_data.push(columnData);
+    }
+    console.log(temp_data);
+    var data_hscmue = {
+        school_code: "HCMUE",
+        max_period: 11,
+        data_schedule: [
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+            {
+                day: "Monday",
+                schedule: [
+                    { time: 1, event: "" },
+                    { time: 2, event: "" },
+                    { time: 3, event: "" },
+                    { time: 4, event: "" },
+                    { time: 5, event: "" },
+                    { time: 6, event: "" },
+                    { time: 7, event: "" },
+                    { time: 8, event: "" },
+                    { time: 9, event: "" },
+                    { time: 10, event: "" },
+                    { time: 10, event: "" },
+                ],
+            },
+        ],
+    };
+    for (let index = 0; index < temp_data.length; index++) {
+        const element = temp_data[index];
+        // console.log(element.length);
+        var i = 0;
+        while (element[i] != undefined) {
+            const e = element[i];
+            var start_end = e[4].split(" ")[1].split("-");
+            var start = start_end[0];
+            var end = start_end[1];
+            console.log(e[0], start, end);
+
+            for (var j = parseInt(start); j < parseInt(end); j++) {
+                // console.log(j)
+                data_hscmue.data_schedule[index].schedule[j].event = e[0];
+            }
+            i++;
+        }
+    }
+
+    mega_data.push(data_hscmue);
+    console.log(data_hscmue);
+}
+
+function docTepJSON() {
+    // Đường dẫn đến tệp JSON nằm trong thư mục gốc của dự án
+    const duongDan = "tuan1.json";
+
+    fetch(duongDan)
+        .then((response) => response.json())
+        .then((data) => {
+            parse_data(data);
+            // console.log(data);
+        })
+        .catch((error) => {
+            console.error("Lỗi khi đọc tệp JSON: ", error);
+        });
+}
+docTepJSON()
